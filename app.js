a7452c4f40e4e4b3d1be72d18f5b8ab4b0d0c52b
Kenji Okimoto <okimoto@clear-code.com>
2015-06-22 16:05:27 +0900

    spec6: Add parseQueryString()

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 var recipe_text = "";
+var parseQueryString = function parseQueryString(options) {
+    var query_string = window.location.search.slice(1);
+    var parameters = {};
+    var pairs = query_string.split("&");
+    for (var i in pairs) {
+        var pair = pairs[i].split("=");
+        parameters[pair[0]] = pair[1];
+    }
+    return parameters;
+};
 
 for (var i = 0; i < recipes.length; i++) {
     var recipe = recipes[i];
