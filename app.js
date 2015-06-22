a1ecead173f950033f748e7e75a868af4121e161
Kenji Okimoto <okimoto@clear-code.com>
2015-06-22 15:47:58 +0900

    spec5: Display recipe with ID

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var recipe_text = "";
 
 for (var i = 0; i < recipes.length; i++) {
     var recipe = recipes[i];
-    recipe_text += recipe + "\n";
+    recipe_text += recipe["id"] + ":" + recipe["name"] + "\n";
 }
 
 alert(recipe_text);
